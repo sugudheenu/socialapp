3e9eedc2ad26cd7f339e775791bc910529035227
Dinesh Ranganathan <dineshranganathan@gmail.com>
2015-06-20 14:59:46 +0100

    Added the first test which is hardcoded to make the assertion pass

--- src/test/groovy/com/sugudheenu/endtoend/CommandlineSocialApplicationSpec.groovy
+++ src/test/groovy/com/sugudheenu/endtoend/CommandlineSocialApplicationSpec.groovy
@@ -1,6 +1,23 @@
 package com.sugudheenu.endtoend
 
+import org.junit.Rule
+import org.junit.contrib.java.lang.system.SystemOutRule
 import spock.lang.Specification
 
 class CommandlineSocialApplicationSpec extends  Specification {
+
+    @Rule
+    public final SystemOutRule output = new SystemOutRule().enableLog();
+
+    def "starts up with a command prompt" () {
+        expect:
+            applicationStartsUpWithPrompt("> ")
+    }
+
+    void applicationStartsUpWithPrompt(String expected) {
+        System.out.print("> ")
+        System.out.flush()
+        assert  output.getLog() == expected
+    }
+
 }
