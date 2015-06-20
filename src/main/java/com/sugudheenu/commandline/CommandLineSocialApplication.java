d0ee696ce83f0ca6f8e1aeb3325ae1a1c4120560
Dinesh Ranganathan <dineshranganathan@gmail.com>
2015-06-20 16:21:20 +0100

    A running application which ignores commands

--- src/main/java/com/sugudheenu/commandline/CommandLineSocialApplication.java
+++ src/main/java/com/sugudheenu/commandline/CommandLineSocialApplication.java
@@ -1,11 +1,20 @@
 package com.sugudheenu.commandline;
 
+import java.util.Scanner;
+
 /**
  * Command line social application
  */
 public class CommandLineSocialApplication {
     public void run() {
+        Scanner scanner = new Scanner(System.in);
         System.out.print("> ");
         System.out.flush();
+        while(scanner.hasNextLine()) {
+            scanner.nextLine();
+            System.out.print("> ");
+            System.out.flush();
+        }
+
     }
 }
--- src/test/groovy/com/sugudheenu/acceptance/CommandLineSocialApplicationSpec.groovy
+++ src/test/groovy/com/sugudheenu/acceptance/CommandLineSocialApplicationSpec.groovy
@@ -3,24 +3,48 @@ package com.sugudheenu.acceptance
 import com.sugudheenu.commandline.CommandLineSocialApplication
 import org.junit.Rule
 import org.junit.contrib.java.lang.system.SystemOutRule
+import org.junit.contrib.java.lang.system.TextFromStandardInputStream
 import spock.lang.Specification
 
+import static org.junit.contrib.java.lang.system.TextFromStandardInputStream.emptyStandardInputStream
+
 class CommandLineSocialApplicationSpec extends Specification {
+    private static final String PROMPT = "> "
+    public static final String EMPTY_COMMAND = ""
+    def application
     @Rule
     public final SystemOutRule output = new SystemOutRule().enableLog();
+    @Rule
+    public final TextFromStandardInputStream input = emptyStandardInputStream()
+
+    def setup() {
+        application = new CommandLineSocialApplication()
+    }
 
     def "starts up with a command prompt"() {
         when:
-            applicationStartsUp()
+            applicationIsRunning()
         then:
-            iCanSee("> ");
+            userCanSee(PROMPT);
     }
 
-    void iCanSee(String expected) {
-        assert output.getLog() == expected
+    def "application is listening to commands"() {
+        when:
+            userEnters(EMPTY_COMMAND, EMPTY_COMMAND)
+            applicationIsRunning()
+        then:
+            userCanSee(PROMPT, PROMPT, PROMPT);
     }
 
-    def applicationStartsUp() {
-        new CommandLineSocialApplication().run();
+    def userEnters(String... command) {
+        input.provideLines(command)
+    }
+
+    void userCanSee(String... expected) {
+        assert output.getLog() == expected.join()
+    }
+
+    def applicationIsRunning() {
+        application.run()
     }
 }
