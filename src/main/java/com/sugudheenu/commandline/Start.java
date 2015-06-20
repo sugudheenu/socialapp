8ae86e3e3dc7942dcf1b7ad6aee01a58f52951c9
Dinesh Ranganathan <dineshranganathan@gmail.com>
2015-06-20 15:00:37 +0100

    Removed untested code from the previous commit

--- src/main/java/com/sugudheenu/commandline/Start.java
+++ src/main/java/com/sugudheenu/commandline/Start.java
@@ -1,16 +1,9 @@
 package com.sugudheenu.commandline;
 
-import java.util.Scanner;
-
 /**
  * Command line social application
  */
 public class Start {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        while(scanner.hasNextLine()) {
-            String line = scanner.nextLine();
-            System.out.println(line);
-        }
     }
 }
