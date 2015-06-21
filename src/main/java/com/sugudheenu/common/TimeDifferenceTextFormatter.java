93a177d6df7c4a0fc9294e9b8f2f9fa288c0d972
Dinesh Ranganathan <dineshranganathan@gmail.com>
2015-06-21 23:22:24 +0100

    Added more time difference formatting as text that gets shown in users timeline

--- src/main/java/com/sugudheenu/common/TimeDifferenceTextFormatter.java
+++ src/main/java/com/sugudheenu/common/TimeDifferenceTextFormatter.java
@@ -3,16 +3,34 @@ package com.sugudheenu.common;
 import java.time.Duration;
 import java.time.Instant;
 
+import static java.time.Duration.between;
 import static java.time.Instant.now;
 
+/**
+ * Utility class that computes time difference
+ */
 public class TimeDifferenceTextFormatter {
-    public static String timeDifferenceAsText(Instant timestamp) {
-        Duration duration = Duration.between(now(), timestamp).abs();
-        long seconds = duration.getSeconds();
-        if (seconds/60 >= 1) {
-            return String.format("%d minute ago", duration.toMinutes());
-        } else {
+
+    /**
+     * @param instant
+     * @return time difference returned as text
+     */
+    public static String timeDifferenceAsText(Instant instant) {
+        Duration duration = between(now(), instant).abs();
+        if (duration.getSeconds() < 1) {
             return "Just Now";
+        } else if (duration.getSeconds() < 60) {
+            return  timeInText(duration.getSeconds(), "second");
+        } else if (duration.toMinutes() < 60) {
+            return  timeInText(duration.toMinutes(), "minute");
+        } else if (duration.toHours() < 24) {
+            return  timeInText(duration.toHours(), "hour");
+        } else {
+            return  timeInText(duration.toDays(), "day");
         }
+    }
+
+    private static String timeInText(long timeUnit, String timeUnitInString) {
+        return String.format("%d %s ago", timeUnit, timeUnit == 1 ? timeUnitInString : timeUnitInString+"s");
     }
 }
--- src/test/groovy/com/sugudheenu/common/TimeDifferenceTextFormatterSpec.groovy
+++ src/test/groovy/com/sugudheenu/common/TimeDifferenceTextFormatterSpec.groovy
@@ -8,12 +8,23 @@ import static java.time.Instant.now
 class TimeDifferenceTextFormatterSpec extends Specification {
 
     @Unroll
-    def "formats #seconds seconds before as #expectedText"() {
+    def "formats #seconds seconds before current time as #expectedText"() {
         expect:
            assert timeDifferenceAsText(now().minusSeconds(seconds)) == expectedText
         where:
             seconds | expectedText
             0       | "Just Now"
+            1       | "1 second ago"
+            2       | "2 seconds ago"
+            59      | "59 seconds ago"
             60      | "1 minute ago"
+            120     | "2 minutes ago"
+            3540    | "59 minutes ago"
+            3600    | "1 hour ago"
+            7200    | "2 hours ago"
+            86399   | "23 hours ago"
+            86400   | "1 day ago"
+            172800  | "2 days ago"
+
     }
 }
